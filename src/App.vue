7fd359f695a22943125a826d04ffdf3f77e952b9
istrzhemechny <i.strzhemechny@tcu.edu>
2025-03-02 17:19:43 -0600

    Lifecycle hooks

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,75 @@
 <template>
-  <div>Hello, Vue!</div>
+  <h1>{{ message }}</h1>
+  <div class="card">
+    <h2 ref="title">This is the App component.</h2>    
+    <h2>Number: {{ number }}</h2>
+    <button @click="number++">Increment number by one</button>
+    <button @click="isShow = !isShow">Toggle Component1</button>
+    <Component1 v-if="isShow"></Component1>
+  </div>
 </template>
 
-<script setup></script>
+<script setup>
+import { 
+  onBeforeMount, 
+  onMounted, 
+  onBeforeUpdate,
+  onUpdated,
+  onBeforeUnmount,
+  onUnmounted,
+  ref, 
+  watch
+} from 'vue'
 
-<style lang="scss" scoped></style>
+import Component1 from './Component1.vue'
+
+let message = ref('Hello, Lifecycle Hooks!')
+
+let number = ref(1)
+const title = ref()
+
+let isShow = ref(true)
+
+console.log('App component is setup')
+
+onBeforeMount(()=>{
+  console.log('App component is before mount.')
+  console.log(number.value)
+  console.log(title.value)
+})
+
+onMounted(()=>{
+  console.log('App component is mounted')
+  console.log(title.value)
+})
+
+onBeforeUpdate(()=>{
+  console.log('App component is before update.')
+})
+
+onUpdated(()=>{
+  console.log('App component is updated')
+})
+
+onBeforeUnmount(()=>{
+  console.log('App component is before unmount')
+})
+
+onUnmounted(()=>{
+  console.log('App component is unmounted')
+})
+
+watch(number, ()=>{
+  console.log('Number changes!')
+})
+
+</script>
+
+<style scoped>
+.card {
+  background-color: purple;
+  color: white;
+  padding: 20px 10px;
+  margin-bottom: 10px;
+}
+</style>
---
+++ src/Component1.vue
@@ -0,0 +1,46 @@
+<template>
+    <div style="background-color: aqua; padding: 10px; margin: 20px">
+      <h2 ref="title">This is the Component1 component.</h2>    
+    </div>
+  </template>
+  
+  <script setup>
+  import { 
+    onBeforeMount, 
+    onMounted, 
+    onBeforeUpdate,
+    onUpdated,
+    onBeforeUnmount,
+    onUnmounted
+  } from 'vue'
+  
+  console.log('Component 1 is setup.')
+  
+  
+  onBeforeMount(()=>{
+    console.log('Component 1 is before mount.')
+  })
+  
+  onMounted(()=>{
+    console.log('Component 1 is mounted')
+  })
+  
+  onBeforeUpdate(()=>{
+    console.log('Component 1 is before update.')
+  })
+  
+  onUpdated(()=>{
+    console.log('Component 1 is updated')
+  })
+  
+  onBeforeUnmount(()=>{
+    console.log('Component 1 is before unmount')
+  })
+  
+  onUnmounted(()=>{
+    console.log('Component 1 is unmounted')
+  })
+  
+  </script>
+  
+  <style scoped></style>
